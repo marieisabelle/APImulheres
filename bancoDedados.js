79751ddb9fb4b7690a10ea0b9243990e7eb663cb
marieisabelle <isabellemarie82@gmail.com>
2024-10-06 22:21:40 -0300

    Correção de comando

--- bancoDedados.js
+++ bancoDedados.js
@@ -5,7 +5,7 @@ async function conectaBancoDeDados() {
    try {
     console.log('Conexão com banco de dados iniciou')
 
-    await mongoose.connect(ProcessingInstruction.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL);
     
     console.log('Conexão com o banco de dados feita com sucesso!')
    } catch(erro) {
